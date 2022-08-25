be534de664fa670b47a56ce2e12dada11e7e7026
Sai Suresh <saisanjanaa1@gmail.com>
2022-08-25 15:57:51 -0700

    add 24

---
+++ Programs/24frame.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Write a program that prints out the position, frame, and letter of the DNA
+# Try coding this with a single loop
+# Try coding this with nested loops
+
+dna = 'ATGGCCTTT'
+
+# single loop
+for i in range(len(dna)):
+	print(str(i) + " " + str(i % 3) + " " + dna[i] + " ")
+	
+# nested loops
+for 
+	
+"""
+python3 24frame.py
+0 0 A
+1 1 T
+2 2 G
+3 0 G
+4 1 C
+5 2 C
+6 0 T
+7 1 T
+8 2 T
+"""

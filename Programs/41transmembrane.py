9c5fa4d747c783caa484788f50556a78e7bf69de
Sai Suresh <saisanjanaa1@gmail.com>
2022-09-14 15:28:43 -0700

    added amphipathic alpha helix function, but still needs debugging

--- Programs/41transmembrane.py
+++ Programs/41transmembrane.py
@@ -15,26 +15,79 @@ import sys
 # Hints:
 #   Create a function for KD calculation
 #   Create a function for amphipathic alpha-helix
+
 amino_acids = ["I", "V", "L", "F", "C", "M", "A", "G", "T", "S", "W", "Y",
-				"P", "H", "E", "Q", "D", "N", "K", "R"]
-hydropathy_scores = [4.5, 4.2, 3.8, 2.8, 2.5, 1.9, 1.8, -0.4, -0.7, -0.9,
-						-1.3, -1.6, -3.2, -3.5, -3.5, -3.5, -3.5, -3.9, -4.5]
-def kd_calc(alpha_helix):
-	KD = 0
-	#print(len(protein))
-	for i in range(0, len(protein)):
-		for j in range(len(amino_acids)):
-			if (protein[i] == amino_acids[j]):
-				KD += hydropathy_scores[j]
-				#print(amino_acids[j], hydropathy_scores[j])
+	"P", "H", "E", "Q", "D", "N", "K", "R"]
+hydropathy_scores = [4.5, 4.2, 3.8, 2.8, 2.5, 1.9, 1.8, -0.4, -0.7, -0.8, 
+	-0.9, -1.3, -1.6, -3.2, -3.5, -3.5, -3.5, -3.5, -3.9, -4.5]
+
+def kd_calc(peptide):
+	sum = 0
+	for i in range(0, len(peptide)):
+		for j in range(0, len(amino_acids)):
+			if (peptide[i] == amino_acids[j]):
+				#print("kd_calc: ", peptide, amino_acids[j], hydropathy_scores[j])
+				sum += hydropathy_scores[j]
+				
 	#print(KD)
-	KD = KD/len(protein)
+	KD = sum/len(peptide)
 	return (KD)
 
 #print(kd_calc("IVLIVLIVLIVAAAAAAA"))
 
 def amphipathic_a_helix(protein):
+	first_30aa = protein[:30]
+	after_30aa = protein[30:]
+	search = False
+	sp = False
+	hydro = False
+	# test signal peptides of len 8 by calculating KD
+	for i in range(0, len(first_30aa),8):
+		signal_peptide = first_30aa[i:i+8]
+		if (len(signal_peptide) == 8):
+			sp_kd = kd_calc(signal_peptide)
+		if (sp_kd > 2.5):
+			#print('found sp! ', signal_peptide)
+			sp = True
+			break
+		else: sp = False
+	# check for hydrophobic regions in same protein
+	for j in range(0,len(after_30aa),11):
+		hydrophobic_peptide = after_30aa[i:i+11]
+		if ( (len(hydrophobic_peptide) == 11) and 'P' not in hydrophobic_peptide):
+			hyd_kd = kd_calc(hydrophobic_peptide)
+			if (hyd_kd > 2.0):
+				#print('found hydrophobic! ',hydrophobic_peptide)
+				hydro = True
+				break
+			else: hydro = False
+	print(sp, hydro)
+	if (sp==True or hydro==True):
+		return True
+		print('amphipathic!')
+	else:
+		return False
+
+
+# read in proteins from file
+
+with open(sys.argv[1]) as fp:
+	lines = fp.readlines()
 	
+	for i in range(len(lines)):
+		if lines[i].startswith('>'):
+			protein_name = lines[i]
+			protein_name = protein_name[0:protein_name.index(' ')]
+			protein_name = protein_name.replace('>','')
+			protein_start = i + 1
+		if lines[i].endswith('*\n'):
+			protein_end = i
+			fullprotein = ''.join(lines[protein_start:protein_end + 1])
+			fullprotein = fullprotein.replace('*\n','')
+			fullprotein = fullprotein.replace('\n','')
+			if (amphipathic_a_helix(fullprotein)):
+				print(protein_name)
+
 """
 python3 40transmembrane.py ../Data/at_prots.fa
 AT1G75120.1
